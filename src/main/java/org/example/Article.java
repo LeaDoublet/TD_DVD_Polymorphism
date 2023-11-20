a64b7d26d5a65e8a2a7acad649ab6bbdf4684892
Léa Doublet <leadbtcode@gmail.com>
2023-11-20 09:38:24 +0100

    update question 1

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,4 +1,37 @@
 package org.example;
 
 public class Article {
+    private String reference;
+    private double prix;
+    private String designation;
+
+    public Article(String reference, double prix, String designation) {
+        this.reference = reference;
+        this.prix = prix;
+        this.designation = designation;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+    // Création d'objets Article pour les tests
+    Article article1 = new Article("A001", 29.99, "Article 1");
+    Article article2 = new Article("A002", 49.99, "Article 2");
+
+    // Affichage des informations des articles
+        System.out.println("Article 1:");
+    afficherDetailsArticle(article1);
+
+        System.out.println("\nArticle 2:");
+    afficherDetailsArticle(article2);
+
+    // Modification des informations d'un article
+        article1.setPrix(39.99);
+        article2.setDesignation("Nouvel article");
+
+    // Affichage des informations mises à jour
+        System.out.println("\nArticle 1 après mise à jour du prix:");
+    afficherDetailsArticle(article1);
+
+        System.out.println("\nArticle 2 après mise à jour de la désignation:");
+    afficherDetailsArticle(article2);
+    }
+
+
+
+// Méthode pour afficher les détails d'un article
+    private static void afficherDetailsArticle(Article article) {
+        System.out.println("Référence: " + article.getReference());
+        System.out.println("Prix: " + article.getPrix());
+        System.out.println("Désignation: " + article.getDesignation());
     }
 }
